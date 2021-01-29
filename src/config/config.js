ba3df4c27aa10e373d818c6b4abe9a13f0f6cbfa
Piyush55dude <piyushagarwalvo@gmail.com>
2021-01-29 17:23:22 +0530

    Made PWA

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-export const img_300 = "http://image.tmdb.org/t/p/w300";
-export const img_500 = "http://image.tmdb.org/t/p/w500";
+export const img_300 = "https://image.tmdb.org/t/p/w300";
+export const img_500 = "https://image.tmdb.org/t/p/w500";
 export const unavailable =
   "https://www.movienewz.com/img/films/poster-holder.jpg";
 export const unavailableLandscape =
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorkerRegistration.register();
